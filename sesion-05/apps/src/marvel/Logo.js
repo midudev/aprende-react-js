111dd90c1a1211b5b8013a5feae3e9ff06d7cb90
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-10-05 18:24:38 +0200

    Add presentacion

--- sesion-05/apps/src/marvel/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Logo () {
-  return (
-    <div className='has-text-centered'>
-      <img className='Logo' role='presentation' src='marvel-logo.jpeg' />
-    </div>
-  )
-}
---
+++ sesion-05/apps/src/marvel_done/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo () {
+  return (
+    <div className='has-text-centered'>
+      <img className='Logo' role='presentation' src='marvel-logo.jpeg' />
+    </div>
+  )
+}
